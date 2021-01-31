591b773e84b6cf1dec327bc053cf67041e383331
tibraj <tibraj94@gmail.com>
2021-01-31 12:27:59 -0500

    max depth solution #2

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ function TreeNode(val, left, right) {
 }
 
 function maxDepth(root) {
-    return !root ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    // return !root ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    if(!root) return 0;
+    let leftDepth = maxDepth(root.left);
+    let rightDepth = maxDepth(root.right);
+    return Math.max(leftDepth + 1, rightDepth + 1);
 }
 
 
