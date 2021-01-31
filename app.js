0c648ad7e7d7a4020fe7d04bce448407ec8e6467
tibraj <tibraj94@gmail.com>
2021-01-31 12:43:58 -0500

    sorted array to bst

--- app.js
+++ app.js
@@ -39,4 +39,19 @@ function singleNumber(nums) {
 
 
 
-//Conver Sorted Array to Binary Search Tree
+//Convert Sorted Array to Binary Search Tree
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+}
+
+function sortedArrayToBST(nums) {
+    if(nums.length === 0) return null;
+    let middle = Math.floor(nums.length / 2);
+    let root = new TreeNode(nums[middle]);
+    root.left = sortedArrayToBST(nums.slice(0, middle));
+    root.right = sortedArrayToBST(nums.slice(middle + 1));
+    return root;
+}
