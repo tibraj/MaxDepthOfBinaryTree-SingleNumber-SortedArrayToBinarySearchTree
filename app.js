f7dbbe2c0921c4935980b0fc2f4d6d9509c72986
tibraj <tibraj94@gmail.com>
2021-01-31 12:37:58 -0500

    single number

--- app.js
+++ app.js
@@ -14,12 +14,27 @@ function maxDepth(root) {
 }
 
 
+// Recursion, the maximum depth is the larger value in the left and 
+// right subtrees, +1 each time the child node is recursive
+
+// Depth-first: Traverse the nodes of the tree along the depth of the tree, 
+// searching for branches of the tree as deep as possible.
+// When the edge of node v has been explored, the search will go back 
+// to the starting node of the edge where node v was found.
+// This process continues until all nodes that have been found reachable 
+// from the source node.
 
 
 
 //Single Number
 
-
+function singleNumber(nums) {
+    let result = 0;
+    for(let i = 0; i < nums.length; i++) {
+        result ^= nums[i];
+    }
+    return result;
+}
 
 
 
